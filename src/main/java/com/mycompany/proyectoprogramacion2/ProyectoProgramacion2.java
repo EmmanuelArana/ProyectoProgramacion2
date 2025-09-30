8fcb86d0a5bf8ccef568723f5d47250eb1125211
Christian <mora@email.com>
2025-09-30 15:17:28 -0600

    Primer commit de Chris

---
+++ src/main/java/com/mycompany/proyectoprogramacion2/ProyectoProgramacion2.java
@@ -0,0 +1,26 @@
+/*
+Software para administrar las ventas de tiquetes en un cine.
+Emmanuel Arana
+Christian Mora
+version 1.0
+14 octubre 2025
+*/
+package com.mycompany.proyectoprogramacion2;
+//modificación de prueba git
+public class ProyectoProgramacion2 {
+
+   public static void main(String[] args) {
+       System.out.println("Bienvenido");
+       System.out.println("Este es un programa de administracion de ventas de un cine.");
+       String nombreCliente;
+       int i=0, entrada2DPrecio=2800, entrada3DPrecio=3200, entrada2DCantidad=0, entrada3DCantidad=0, cedula;
+       float impuesto=0.13, montoCobro=0.0, resultadoCalculoIva=0.0;
+       //se establece un limite de 500 tiquetes.
+       for(i=0,i<500,i++){
+          
+           
+           
+       }
+
+   }
+}
